3731c29ff77a77589243e6d8dad80fba4550cb9a
VictorFu <e24082117@gs.ncku.edu.tw>
2025-03-05 23:01:03 +0800

    Implement q_delete_mid & q_delete_dup
    Remove a middle node from the queue/duplicate nodes from sorted one.
    
    Change-Id: I381368eeba8b8f903fd5228ecad57f5864e49e5c

--- queue.c
+++ queue.c
@@ -64,19 +64,45 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head || !s) {
+        return false;
+    }
+    element_t *new_element = (element_t *) malloc(sizeof(element_t));
+    if (!new_element) {
+        return false;
+    }
+    new_element->value = malloc(strlen(s) + 1);
+    if (!new_element->value) {
+        free(new_element);
+        return false;
+    }
+    strlcpy(new_element->value, s, strlen(s) + 1);
+    list_add_tail(&new_element->list, head);
     return true;
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head) || !sp || bufsize == 0)
+        return NULL;
+    struct list_head *remove_node = head->next;
+    element_t *remove_element = list_entry(remove_node, element_t, list);
+    strlcpy(sp, remove_element->value, bufsize);
+    list_del(remove_node);
+    return remove_element;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head) || !sp || bufsize == 0)
+        return NULL;
+    struct list_head *remove_node = head->prev;
+    element_t *remove_element = list_entry(remove_node, element_t, list);
+    strlcpy(sp, remove_element->value, bufsize);
+    list_del(remove_node);
+    return remove_element;
 }
 
 /* Return number of elements in queue */
@@ -105,35 +131,7 @@ bool q_delete_mid(struct list_head *head)
 /* Delete all nodes that have duplicate string */
 bool q_delete_dup(struct list_head *head)
 {
-    if (!head || list_empty(head)) {
-        return false;
-    }
-    struct list_head *node, *safe;
-    bool isduplicate = false;
-    list_for_each_safe(node, safe, head) {
-        element_t *cur = list_entry(node, element_t, list);
-        if (safe != head) {
-            const element_t *nex = list_entry(safe, element_t, list);
-            if (!strcmp(cur->value, nex->value)) {
-                list_del(node);
-                free(cur->value);
-                free(cur);
-                isduplicate = true;
-            } else if (isduplicate) {
-                list_del(node);
-                free(cur->value);
-                free(cur);
-                isduplicate = false;
-            }
-        } else {
-            if (isduplicate) {
-                list_del(node);
-                free(cur->value);
-                free(cur);
-                isduplicate = false;
-            }
-        }
-    }
+    // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
     return true;
 }
 
